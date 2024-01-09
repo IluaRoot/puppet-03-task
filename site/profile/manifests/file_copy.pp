d9c25e5b5b8732ba45f0fe09ee4b8cd1853daee5
L Ilua <theykilledteddybear@gmail.com>
2024-01-09 21:32:11 +0300

    feature

--- site/profile/manifests/file_copy.pp
+++ site/profile/manifests/file_copy.pp
@@ -1,6 +1,6 @@
 class profile::file_copy (
     String $pathname = '/etc/nginx/conf.d/static.conf',
-    String $sourcefile = 'puppet:///manifests/static.conf',
+    String $sourcefile = 'puppet:///manifests/files/static.conf',
 ) {
   file { 'Copy conf file':
     ensure => file,
