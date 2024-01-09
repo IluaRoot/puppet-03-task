17b7cc677f814e85a82523824e362b55e2be9cfc
L Ilua <theykilledteddybear@gmail.com>
2024-01-09 20:57:10 +0300

    Changing for testing

--- site/profile/manifests/file_copy.pp
+++ site/profile/manifests/file_copy.pp
@@ -2,8 +2,9 @@ class profile::file_copy (
     String $pathname = '/etc/nginx/conf.d/static.conf',
     String $sourcefile = '/vagrant/conf.f/static.conf',
 ) {
-  file { $pathname:
+  file { 'Copy conf file':
     ensure => file,
+    path => $pathname,
     source => $sourcefile,
  }
 }
