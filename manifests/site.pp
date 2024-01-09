83f8a7a75f1a7f42ce7bb5b2b8f85703a7cd5f64
L Ilua <theykilledteddybear@gmail.com>
2024-01-09 21:02:45 +0300

    Slave2 testing 2

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ node 'slave1.puppet' {
 node 'slave2.puppet' {
    include profile::package_install
    class { 'profile::file_copy':
-      path => '/etc/nginx/conf.d/dynamic',
-      source => '/vagrant/conf.f/dynamic.conf',
+      $pathname = '/etc/nginx/conf.d/dynamic',
+      $sourcefile = '/vagrant/conf.f/dynamic.conf',
    }
 }
