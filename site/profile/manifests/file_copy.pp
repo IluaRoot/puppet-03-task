6ef5fa33381c1f281e20ecc3fa45a52887bc6473
L Ilua <theykilledteddybear@gmail.com>
2024-01-09 20:50:43 +0300

    editing copy file

--- site/profile/manifests/file_copy.pp
+++ site/profile/manifests/file_copy.pp
@@ -1,6 +1,6 @@
 class profile::file_copy (
     String $pathname = '/etc/nginx/conf.d/static.conf',
-    String $sourcefile = 'puppet:///files/static.conf',
+    String $sourcefile = 'puppet:///static.conf',
 ) {
   file { $pathname:
     ensure => file,
