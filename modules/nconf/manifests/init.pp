9ff647ccd2e9a340c01fc3fcc96734cc6d9277de
L Ilua <theykilledteddybear@gmail.com>
2024-01-11 21:32:27 +0300

    Modules wip16

--- modules/nconf/manifests/init.pp
+++ modules/nconf/manifests/init.pp
@@ -3,7 +3,7 @@ class nconf {
      ensure => absent,
    }
      file { 'Copy Default Conf':
-     path => '/etc/nginx/nginx.conf',
+     path => '/etc/nginx/',
      ensure => file,
      source => 'puppet:///modules/nconf/nginx',
    }  
