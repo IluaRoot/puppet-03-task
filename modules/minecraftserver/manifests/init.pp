38a27e3796cbb4334892ec2f806aba139e0c9e0a
L Ilua <theykilledteddybear@gmail.com>
2024-01-15 13:50:47 +0300

    4 task wip 2.2.3

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -10,22 +10,33 @@ class minecraftserver {
 
    file { 'Service file copy':
      ensure => file,
-     path => '/etc/systemd/system/minecraftserver.service',
+     path   => '/opt/minecraft/minecraftserver.service',
      source => 'puppet:///modules/minecraftserver/minecraftserver.service',
+   }
+
+   file { '/opt/minecraft/minecraftserver.service':
+     ensure => '/etc/systemd/system/minecraftserver.service',
    }
 
    remote_file { 'Download minecraft server':
      ensure => present,
-     path => '/opt/minecraft/minecraft_server.1.20.4.jar',
+     path   => '/opt/minecraft/minecraft_server.1.20.4.jar',
      source => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',     
    }
 
    service { 'minecraftserver':
-     ensure => running,
-     enable => true,
+     ensure  => running,
+     enable  => true,
      require => [
        File['/opt/minecraft'],
        Remote_file['Download minecraft server'],
        ]
+   }
+
+   file_line { 'Agree with minecraft eula':
+      ensure => present,
+      path   => '/opt/minecraft/eula.txt',
+      line   => "eula=true",
+      match  => 'eula=false',
    }
 }
